de938c855d9269a0a7a139124bde92487400fe7e
Cheikh Ibra YADE <61935826+7h1ck@users.noreply.github.com>
2020-07-14 16:19:56 +0000

    Add files via upload

---
+++ Service.java
@@ -0,0 +1,64 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Service implements IService{
+
+   //Auto généré
+   protected static int gid=0;
+    private int id;
+    private String libelle;
+    private ArrayList<Embauche>tabEmbauchee;
+ 
+    //Constructeur
+    public Service(){
+      tabEmbauchee = new ArrayList<>();
+       Service.gid++;
+       this.id=Service.gid;
+
+    }
+
+    public Service(String libelle){
+       this.libelle=libelle;
+       tabEmbauchee = new ArrayList<>();
+       Service.gid++;
+       this.id=Service.gid;
+   }
+
+
+   //Getters and Setters
+   // public String getNom(){
+   //    return libelle;
+   // }
+
+   //UC
+   public void addEmployer(Embauche embauche){
+      tabEmbauchee.add(embauche);
+
+   }
+
+
+   public void listeEmbauche(){
+      for(Embauche e : tabEmbauchee){
+          System.out.println(e.affiche());
+      }
+      
+  }
+//   public void compare(int id){
+   
+   
+// }
+// public void compare(String libelle){
+   
+   
+// }
+  
+   
+
+   //redefinition de la methode affiche()
+
+    @Override
+    public String affiche(){
+       return "Id: "+this.id+" Nom : "+this.libelle;
+    }
+
+}
